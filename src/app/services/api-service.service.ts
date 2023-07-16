91d72ae060e57092b2c1b8cc27f216fb6be5f037
Dawn Kelly <dawnkelly09@gmail.com>
2023-07-16 16:45:59 -0400

    rough html/css skeleton in place

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -7,3 +7,8 @@ export class ApiServiceService {
 
   constructor() { }
 }
+
+/*Need to create two functions
+1) one method that accepts a country name as an input parameter that returns additional information gathered from the API for the selected country
+2) one method that will trigger the API call when a country is selected and set a local variable that will receive the information about the country for display in the right column of the HTML page
+
